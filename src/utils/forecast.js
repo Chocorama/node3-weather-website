3ea5bb8b32e1da7ed159e60b6f48ae0585061294
Willem <willemhumphrey@gmail.com>
2020-10-02 14:21:02 -0700

    changed <p> tag to <h3> and some basic wording of forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,16 +4,7 @@ const forecast = (lat, long, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=ae12844a3452bbdb1072fb6eae07ddb0&query=${long},${lat}&units=f`;
 
   request({ url, json: true }, (error, response) => {
-    const geetGoat = (num) => {
-      if (num < 70) {
-        return 'Bring geet feathers';
-      } else {
-        return 'Leave feathers at Home';
-      }
-    };
-
     const { current } = response.body;
-    console.log(current);
     if (error) {
       callback('Unable to connect to network or online service', undefined);
     } else if (response.body.error) {
@@ -25,14 +16,11 @@ const forecast = (lat, long, callback) => {
       callback(
         undefined,
         `It is currently ${
-          current.temperature
-        } degrees out in the feather zone and ${
+          current.is_day === 'yes' ? 'day-time' : 'night-time'
+        } and ${current.temperature} degrees out with ${
           current.weather_descriptions[0]
-        }. It feels like ${
-          current.feelslike
-        } degrees out and too hot for goating around. ${geetGoat(
-          current.temperature
-        )}`
+        }. 
+        It feels like ${current.feelslike} degrees out.`
       );
     }
   });
